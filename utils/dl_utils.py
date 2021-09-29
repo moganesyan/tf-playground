fixed hard sigmoid, added scaling to mobilenetv3 and tweaked test notebook

utils/dl_utils.py
@@ -15,22 +15,6 @@ class ReLU6(layers.Layer):
         return x_out
 
 
-class HSwish(layers.Layer):
-    """
-        Hard SWISH function from the MobilenetV3 paper
-    """
-
-    def __init__(self) -> None:
-        super(HSwish, self).__init__()
-
-    def call(self, x_in: layers.Layer) -> layers.Layer:
-        x = tf.add(x_in, 3.0)
-        x = ReLU6()(x)
-        x = tf.multiply(x, 1/6)
-        x_out = layers.Multiply()([x_in, x]) 
-        return x_out
-
-
 class HSigm(layers.Layer):
     """
         Hard Sigmoid from the MobilenetV3 paper
@@ -41,5 +25,20 @@ class HSigm(layers.Layer):
 
     def call(self, x_in: layers.Layer) -> layers.Layer:
         x = tf.add(x_in, 3.0)
-        x_out = tf.clip_by_value(x, 0.0, 1.0)
+        x = ReLU6()(x)
+        x_out = tf.multiply(x, 1/6)
+        return x_out
+
+
+class HSwish(layers.Layer):
+    """
+        Hard SWISH function from the MobilenetV3 paper
+    """
+
+    def __init__(self) -> None:
+        super(HSwish, self).__init__()
+
+    def call(self, x_in: layers.Layer) -> layers.Layer:
+        x = HSigm()(x_in)
+        x_out = layers.Multiply()([x_in, x]) 
         return x_out