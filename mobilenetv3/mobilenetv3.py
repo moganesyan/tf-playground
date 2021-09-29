270621e6498ebabe130d06726e547e171d39da01
moganesyan <mishaogan1@hotmail.com>
2021-09-29 03:51:51 +0100

    mobilenet v3 running

--- mobilenetv3/mobilenetv3.py
+++ mobilenetv3/mobilenetv3.py
@@ -4,52 +4,50 @@ from typing import Optional, Tuple, Union, List, Dict
 import tensorflow as tf
 from tensorflow.keras import layers
 from tensorflow.keras import models
+from tensorflow.keras.layers import ReLU
 
-# from utils.dl_utils import ReLU6
-
-
-import tensorflow as tf
-from tensorflow.keras import layers
-
-
-class ReLU6(layers.Layer):
-    """
-        Custom ReLU6 activation layer
-    """
-
-    def __init__(self) -> None:
-        super(ReLU6, self).__init__()
-
-    def call(self, input) -> layers.Layer:
-        return tf.clip_by_value(
-            input,
-            clip_value_min = 0.0,
-            clip_value_max = 6.0)
-            
+from utils.dl_utils import ReLU6, HSwish, HSigm
 
 
 MOBILENETV3_LARGE = [
-    {'type': 'conv2d', 'k': 3, 'exp': None, 'nout': 16, 'se': False, 'nl': ReLU6, 'bn': True, 's': 2}, #
-    {'type': 'bneck', 'k': 3, 'exp': 16, 'nout': 16, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 64, 'nout': 24, 'se': False, 'nl': ReLU6, 'bn': True, 's': 2}, #
-    {'type': 'bneck', 'k': 3, 'exp': 72, 'nout': 24, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 5, 'exp': 72, 'nout': 40, 'se': True, 'nl': ReLU6, 'bn': True, 's': 2}, #
-    {'type': 'bneck', 'k': 5, 'exp': 120, 'nout': 40, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 5, 'exp': 120, 'nout': 40, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 240, 'nout': 80, 'se': False, 'nl': ReLU6, 'bn': True, 's': 2}, #
-    {'type': 'bneck', 'k': 3, 'exp': 200, 'nout': 80, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 184, 'nout': 80, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 184, 'nout': 80, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 480, 'nout': 112, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 3, 'exp': 672, 'nout': 160, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 5, 'exp': 672, 'nout': 160, 'se': True, 'nl': ReLU6, 'bn': True, 's': 2}, #
-    {'type': 'bneck', 'k': 5, 'exp': 960, 'nout': 160, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'bneck', 'k': 5, 'exp': 960, 'nout': 160, 'se': True, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 960, 'se': False, 'nl': ReLU6, 'bn': True, 's': 1}, #
-    {'type': 'pool', 'k': 7, 'exp': None, 'nout': None, 'se': False, 'nl': None, 'bn': False, 's': 1}, #
-    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 1280, 'se': False, 'nl': ReLU6, 'bn': False, 's': 1}, #
+    {'type': 'conv2d', 'k': 3, 'exp': None, 'nout': 16, 'se': False, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 16, 'nout': 16, 'se': False, 'nl': ReLU, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 64, 'nout': 24, 'se': False, 'nl': ReLU, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 72, 'nout': 24, 'se': False, 'nl': ReLU, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 72, 'nout': 40, 'se': True, 'nl': ReLU, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 5, 'exp': 120, 'nout': 40, 'se': True, 'nl': ReLU, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 120, 'nout': 40, 'se': True, 'nl': ReLU, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 240, 'nout': 80, 'se': False, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 200, 'nout': 80, 'se': False, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 184, 'nout': 80, 'se': False, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 184, 'nout': 80, 'se': False, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 480, 'nout': 112, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 3, 'exp': 672, 'nout': 160, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 672, 'nout': 160, 'se': True, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 5, 'exp': 960, 'nout': 160, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 960, 'nout': 160, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 960, 'se': False, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'pool', 'k': 7, 'exp': None, 'nout': None, 'se': False, 'nl': None, 'bn': None, 's': 1}, #
+    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 1280, 'se': False, 'nl': HSwish, 'bn': False, 's': 1}, #
 ]
 
+MOBILENETV3_SMALL = [
+    {'type': 'conv2d', 'k': 3, 'exp': None, 'nout': 16, 'se': False, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 16, 'nout': 16, 'se': True, 'nl': ReLU, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 72, 'nout': 24, 'se': False, 'nl': ReLU, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 3, 'exp': 88, 'nout': 24, 'se': False, 'nl': ReLU, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 96, 'nout': 40, 'se': True, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 5, 'exp': 240, 'nout': 40, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 240, 'nout': 40, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 120, 'nout': 48, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 144, 'nout': 48, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 288, 'nout': 96, 'se': True, 'nl': HSwish, 'bn': True, 's': 2}, #
+    {'type': 'bneck', 'k': 5, 'exp': 576, 'nout': 96, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'bneck', 'k': 5, 'exp': 576, 'nout': 96, 'se': True, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 576, 'se': False, 'nl': HSwish, 'bn': True, 's': 1}, #
+    {'type': 'pool', 'k': 7, 'exp': None, 'nout': None, 'se': False, 'nl': None, 'bn': None, 's': 1}, #
+    {'type': 'conv2d', 'k': 1, 'exp': None, 'nout': 1024, 'se': False, 'nl': HSwish, 'bn': False, 's': 1}, #
+]
 
 
 class MobilenetV3():
@@ -57,10 +55,51 @@ class MobilenetV3():
         Mobilenet V3 Base class
     """
 
-    def __init__(self, architecture: List[Dict]) -> None:
-        self._architecture: List[Dict] = architecture
+    def __init__(self,
+                 input_shape: Tuple[int, int, int],
+                 output_size: int,
+                 alpha: float = 1.0,
+                 size: str = 'large') -> None:
+
+        self._input_shape: Tuple[int, int, int] = input_shape
+        self._output_size: int = output_size
+        self._alpha: float = alpha
         #
-        self._se_factor = 16
+        self._se_factor: int = 4
+        self._se_type: str = 'conv'
+        #
+        assert size in ['small', 'large'], "use 'small' or 'large'"
+        self._size: str = size
+        self._architecture: List[Dict]  = MOBILENETV3_LARGE if size == 'large' else MOBILENETV3_SMALL
+
+    def _se_block(self, x_in: layers.Layer) -> layers.Layer:
+        """
+            Squeeze and excitation block
+        """
+
+        # FC variant of the SE block
+        if self._se_type == 'fc':
+            x = x_in
+            x = layers.GlobalAveragePooling2D()(x)
+            dim_x = x.shape[1]
+            x = layers.Dense(int(floor(dim_x / self._se_factor)))(x)
+            x = ReLU()(x)
+            x = layers.Dense(dim_x)(x)
+            x = HSigm()(x)
+            x_out = layers.Multiply()([x_in, x])
+            return x_out
+        # Conv variant of the SE block
+        elif self._se_type == 'conv':
+            x = x_in
+            x = layers.GlobalAveragePooling2D()(x)
+            dim_x = x.shape[1]
+            x = layers.Reshape((1,1,dim_x))(x)
+            x = layers.Conv2D(int(floor(dim_x / self._se_factor)), 1,1,'same')(x)
+            x = ReLU()(x)
+            x = layers.Conv2D(dim_x, 1,1,'same')(x)
+            x = HSigm()(x)
+            x_out = layers.Multiply()([x_in, x])
+            return x_out
 
     def _inverted_residual_block_v3(self, x_in: layers.Layer, k: int,
                                     exp: int, nout: int, se: bool,
@@ -81,29 +120,16 @@ class MobilenetV3():
         x = nl()(x)
         # optional SE block
         if se:
-            x_se = x
-            x_se = layers.GlobalAveragePooling2D()(x_se)
-            dim_x_se = x_se.shape[1]
-            x_se = layers.Dense(int(floor(dim_x_se / self._se_factor)))(x_se)
-            x_se = nl()(x_se)
-            x_se = layers.Dense(dim_x_se)(x_se)
-            x_se = tf.nn.sigmoid(x_se) # placeholder
-            x = layers.Multiply()([x, x_se])
+            x = self._se_block(x)
         # projection
         x = layers.Conv2D(nout,1,1,'same')(x)
         if bn:
             x = layers.BatchNormalization()(x)
-        x = nl()(x)
-        # residual block and output
-        if x_in.shape[3] == nout:
-            identity = x_in
-        else:
-            identity = layers.Conv2D(nout, 1, 1, 'same')(x_in)
-            if bn:
-                identity = layers.BatchNormalization()(identity)
-        if s == 2:
+        # residual connection and output
+        if s == 2 or x_in.shape[3] != nout:
             x_out = x
         else:
+            identity = x_in
             x_out = layers.Add()([x, identity])
         return x_out
 
@@ -126,26 +152,10 @@ class MobilenetV3():
         """
             Pool block constructor
         """
-        x_out = layers.AveragePooling2D(k,s,'valid')(x_in)
+
+        x = layers.GlobalAveragePooling2D()(x_in)
+        x_out = layers.Reshape((1,1,x_in.shape[3]))(x)
         return x_out
-
-
-class MobilenetV3Large(MobilenetV3):
-    """
-        Mobilenet V3 Large sub-Class
-    """
-
-    def __init__(self,
-                 input_shape: Tuple[int, int, int],
-                 n_classes: int,
-                 alpha: float = 1.0) -> None:
-        """
-            TODO: expand this
-        """
-        super().__init__(MOBILENETV3_LARGE)
-        self._input_shape: Tuple[int, int, int] = input_shape
-        self._n_classes: int = n_classes
-        self._alpha: float = alpha
 
     def _build_network(self) -> Union[layers.Layer, layers.Layer]:
         """
@@ -155,7 +165,7 @@ class MobilenetV3Large(MobilenetV3):
         x_in = layers.Input(self._input_shape)
 
         x = x_in
-        for layer_idx, layer_dict in enumerate(self._architecture):
+        for _, layer_dict in enumerate(self._architecture):
             if layer_dict['type'] == 'conv2d':
                 x = self._conv2d_block(
                     x, layer_dict['k'], layer_dict['exp'], layer_dict['nout'],
@@ -172,8 +182,10 @@ class MobilenetV3Large(MobilenetV3):
                     layer_dict['se'], layer_dict['nl'], layer_dict['bn'],
                     layer_dict['s'])
 
-        x_out = layers.Conv2D(self._n_classes,1,1,'valid')(x)
-        x_out = layers.Softmax()(x_out)
+        x = layers.Dropout(0.20)(x)
+        x = layers.Conv2D(self._output_size,1,1,'valid')(x)
+        x = layers.Flatten()(x)
+        x_out = layers.Softmax()(x)
         return x_in, x_out
 
     def __call__(self) -> models.Model:
@@ -183,13 +195,4 @@ class MobilenetV3Large(MobilenetV3):
 
         x_in, x_out = self._build_network()
         model = models.Model(inputs = x_in, outputs = x_out)
-
         return model
-
-
-
-mobilenetv3 = MobilenetV3Large((224,224,3),10)
-model = mobilenetv3()
-
-tf.keras.utils.plot_model(model, "my_first_model_with_shape_info.png", show_shapes=True)
-model.save('mobilenetv3.h5')
