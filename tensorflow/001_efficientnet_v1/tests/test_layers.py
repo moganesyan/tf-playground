5d1f2d326801e60a9690826bdd2b2c2ff89ce0f0
moganesyan <mishaogan1@hotmail.com>
2021-11-24 23:09:51 +0000

    added conv2d layer and wrote tests

--- tensorflow/001_efficientnet_v1/tests/test_layers.py
+++ tensorflow/001_efficientnet_v1/tests/test_layers.py
@@ -1,10 +1,10 @@
+from typing import Union
 import unittest
 import tensorflow as tf
 
-from utils import Dense
+from utils import Dense, Conv2D
 
 
-# Test Dense Layer
 class TestDenseLayer(unittest.TestCase):
     """
         Test cases for the dense layer.
@@ -16,10 +16,70 @@ class TestDenseLayer(unittest.TestCase):
         """
 
         x_in = tf.constant(
-            tf.random.normal((224,224,3)),
+            tf.random.normal((1,224,224,3)),
             dtype = tf.float32
         )
         layer = Dense(100, "dense_test")
         x_out = layer(x_in)
 
-        assert x_out.shape == (224,224,100)
+        assert x_out.shape == (1,224,224,100)
+
+
+class TestConv2DLAYER(unittest.TestCase):
+    """
+        Test cases for the Conv2D layer.
+    """
+
+    def test_output_shape(self):
+        """
+            Test the shape of the conv2d layer's output tensor.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = Conv2D(
+            32, 3, 1, 'same', name = "conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,224,224,32)
+
+    def test_valid_padding(self):
+        """
+            Test 'valid' padding type.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = Conv2D(
+            32, 3, 1, 'valid', name = "conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,222,222,32)
+
+    def test_stride(self):
+        """
+            Test stride.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = Conv2D(
+            32, 3, 2, 'same', name = "conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,112,112,32)  
--- tensorflow/001_efficientnet_v1/utils.py
+++ tensorflow/001_efficientnet_v1/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 import tensorflow as tf
 
 
@@ -12,6 +12,7 @@ class Dense(tf.Module):
                  name: Optional[str] = None):
 
         super(Dense, self).__init__(name)
+
         self.neurons: int = neurons
         self.is_built: bool = False
 
@@ -32,14 +33,81 @@ class Dense(tf.Module):
                 name = "dense_weights"
             )
             self.b = tf.Variable(
-                tf.random.normal([self.neurons]),
+                tf.random.normal((self.neurons,)),
                 trainable = True,
                 dtype = tf.float32,
                 name = "dense_bias"
             )
-            print(self.W)
-            print(self.b)
+
             self._is_built = True
 
-        x_out = tf.matmul(x_in, self.W) + self.b
-        return x_out
+        return tf.matmul(x_in, self.W) + self.b
+
+
+class Conv2D(tf.Module):
+    """
+        2D Convolution Layer.
+    """
+
+    def __init__(self,
+                 filters_out: int,
+                 kernel: Union[int, Tuple[int, int]],
+                 stride: Union[int, Tuple[int, int]],
+                 padding: str,
+                 dilation: Union[int, Tuple[int, int]] = 1,
+                 data_format: str = "channels_last",
+                 use_bias: bool = True,
+                 name = None) -> None:
+
+        super(Conv2D, self).__init__(name)
+
+        self.filters_out: int = filters_out
+        self.kernel: Union[int, Tuple[int, int]] = (kernel
+            if isinstance(kernel, Tuple) else (kernel, kernel))
+        self.stride: Union[int, Tuple[int, int]] = (stride
+            if isinstance(stride, Tuple) else (stride, stride))
+        self.padding: str = padding.upper()
+        self.dilation: Union[int, Tuple[int, int]] = (dilation
+            if isinstance(dilation, Tuple) else (dilation, dilation))
+        self.data_format: str = ("NHWC"
+            if data_format == "channels_last" else "NCHW")
+        self.use_bias: bool = use_bias
+
+        self.is_built: bool = False
+
+        self.W: Optional[tf.Variable] = None
+        self.b: Optional[tf.Variable] = None
+
+    def __call__(self, x_in: tf.Tensor) -> tf.Tensor:
+        """
+            Build tensor on the first call.
+            Calculate output by 2D convolution.
+        """
+
+        if not self.is_built:
+            filters_in = x_in.shape[-1]
+            filter_weights = self.kernel + (filters_in, self.filters_out)
+
+            self.W = tf.Variable(
+                tf.random.normal(filter_weights),
+                trainable = True,
+                dtype = tf.float32,
+                name = "conv2d_filters"
+            )
+            self.b = tf.Variable(
+                tf.random.normal((self.filters_out,)),
+                trainable = True,
+                dtype = tf.float32,
+                name = "conv2d_bias"
+            )
+
+            if self.use_bias:
+                return tf.nn.conv2d(
+                    x_in, self.W, self.stride, self.padding,
+                    self.data_format, self.dilation, name = "conv2d_conv"
+                ) + self.b
+            else:
+                return tf.nn.conv2d(
+                    x_in, self.W, self.stride, self.padding,
+                    self.data_format, self.dilation, name = "conv2d_conv"
+                )
