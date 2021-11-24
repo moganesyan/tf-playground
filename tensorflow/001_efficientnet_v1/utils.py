15f436b06f3bf102c7b22e0a4dd24b8b6374aef9
moganesyan <mishaogan1@hotmail.com>
2021-11-24 22:10:07 +0000

    style changes to Dense Layer

--- tensorflow/001_efficientnet_v1/utils.py
+++ tensorflow/001_efficientnet_v1/utils.py
@@ -12,32 +12,34 @@ class Dense(tf.Module):
                  name: Optional[str] = None):
 
         super(Dense, self).__init__(name)
-        self._neurons: int = neurons
-        self._is_built: bool = False
+        self.neurons: int = neurons
+        self.is_built: bool = False
 
         self.W: Optional[tf.Variable] = None
         self.b: Optional[tf.Variable] = None
 
-    def __call__(self, x: tf.Tensor) -> tf.Tensor:
+    def __call__(self, x_in: tf.Tensor) -> tf.Tensor:
         """
             Build tensor on the first call.
             Calculate output by matrix multiplication.
         """
 
-        if not self._is_built:
+        if not self.is_built:
             self.W = tf.Variable(
-                tf.random.normal((x.shape[-1], self._neurons)),
+                tf.random.normal([x_in.shape[-1], self.neurons]),
                 trainable = True,
                 dtype = tf.float32,
                 name = "dense_weights"
             )
             self.b = tf.Variable(
-                tf.random.normal((1, self._neurons)),
+                tf.random.normal([self.neurons]),
                 trainable = True,
                 dtype = tf.float32,
                 name = "dense_bias"
             )
+            print(self.W)
+            print(self.b)
             self._is_built = True
 
-        x_out = tf.matmul(x, self.W) + self.b
+        x_out = tf.matmul(x_in, self.W) + self.b
         return x_out
