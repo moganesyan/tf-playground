62e9c85cbad2f0f59a45674ff64be5779c443264
moganesyan <mishaogan1@hotmail.com>
2021-12-23 18:00:41 +0300

    added depthwise convolution layer

--- tensorflow/001_efficientnet_v1/tests/test_layers.py
+++ tensorflow/001_efficientnet_v1/tests/test_layers.py
@@ -2,7 +2,7 @@ from typing import Union
 import unittest
 import tensorflow as tf
 
-from utils import Dense, Conv2D
+from utils import Dense, Conv2D, DepthwiseConv2D
 
 
 class TestDenseLayer(unittest.TestCase):
@@ -32,7 +32,7 @@ class TestConv2DLayer(unittest.TestCase):
 
     def test_output_shape(self):
         """
-            Test the shape of the conv2d layer's output tensor.
+            Test the output shape.
         """
 
         x_in = tf.constant(
@@ -105,3 +105,105 @@ class TestConv2DLayer(unittest.TestCase):
         x_out = layer(x_in)
 
         assert x_out.shape == (1,32,224,224)
+
+
+class TestDepthwiseConv2DLayer(unittest.TestCase):
+    """
+        Test cases for the Depthwise Conv2D layer.
+    """
+
+    def test_output_shape(self):
+        """
+            Test the output shape.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = DepthwiseConv2D(
+            3, 1, 'same', name = "dwise_conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,224,224,3)
+
+    def test_depth_multiplier(self):
+        """
+            Test the depth multiplier.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = DepthwiseConv2D(
+            3, 1, 'same',
+            depth_multiplier = 2,
+            name = "dwise_conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,224,224,6)
+
+    def test_valid_padding(self):
+        """
+            Test 'valid' padding type.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = DepthwiseConv2D(
+            3, 1, 'valid', name = "dwise_conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,222,222,3)
+
+    def test_stride(self):
+        """
+            Test stride.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,224,224,3)),
+            dtype = tf.float32
+        )
+
+        layer = DepthwiseConv2D(
+            3, 2, 'same', name = "dwise_conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,112,112,3)
+
+    @unittest.skip
+    def test_output_shape_inverted(self):
+        """
+            Test the shape of the conv2d layer's output tensor.
+            NCHW format.
+        """
+
+        x_in = tf.constant(
+            tf.random.normal((1,3,224,224)),
+            dtype = tf.float32
+        )
+
+        layer = DepthwiseConv2D(
+            3, 1, 'same',
+            data_format = "channels_first",
+            name = "dwise_conv2d_test"
+        )
+
+        x_out = layer(x_in)
+
+        assert x_out.shape == (1,3,224,224)
--- tensorflow/001_efficientnet_v1/utils.py
+++ tensorflow/001_efficientnet_v1/utils.py
@@ -62,12 +62,12 @@ class Conv2D(tf.Module):
         super(Conv2D, self).__init__(name)
 
         self.filters_out: int = filters_out
-        self.kernel: Union[int, Tuple[int, int]] = (kernel
+        self.kernel: Tuple[int, int] = (kernel
             if isinstance(kernel, Tuple) else (kernel, kernel))
-        self.stride: Union[int, Tuple[int, int]] = (stride
+        self.stride: Tuple[int, int] = (stride
             if isinstance(stride, Tuple) else (stride, stride))
         self.padding: str = padding.upper()
-        self.dilation: Union[int, Tuple[int, int]] = (dilation
+        self.dilation: Tuple[int, int] = (dilation
             if isinstance(dilation, Tuple) else (dilation, dilation))
         self.data_format: str = ("NHWC"
             if data_format == "channels_last" else "NCHW")
@@ -112,4 +112,76 @@ class Conv2D(tf.Module):
             return tf.nn.conv2d(
                 x_in, self.W, self.stride, self.padding,
                 self.data_format, self.dilation, name = "conv2d_conv"
+            )
+
+
+class DepthwiseConv2D(tf.Module):
+    """
+        2D Depthwise Convolution Layer.
+    """
+
+    def __init__(self,
+                 kernel: Union[int, Tuple[int, int]],
+                 stride: Union[int, Tuple[int, int, int, int]],
+                 padding: str,
+                 dilation: Union[int, Tuple[int, int]] = 1,
+                 depth_multiplier: int = 1,
+                 data_format: str = "channels_last",
+                 use_bias: bool = True,
+                 name = None) -> None:
+
+        super(DepthwiseConv2D, self).__init__(name)
+
+        self.kernel: Tuple[int, int] = (kernel
+            if isinstance(kernel, Tuple) else (kernel, kernel))
+        stride_tuple = ((1, stride, stride, 1)
+            if data_format == "channels_last" else (1, 1, stride, stride))
+        self.stride: Tuple[int, int, int, int ] = (stride
+            if isinstance(stride, Tuple) else stride_tuple)
+        self.padding: str = padding.upper()
+        self.dilation: Tuple[int, int] = (dilation
+            if isinstance(dilation, Tuple) else (dilation, dilation))
+        self.depth_multiplier: int = depth_multiplier
+        self.data_format: str = ("NHWC"
+            if data_format == "channels_last" else "NCHW")
+        self.use_bias: bool = use_bias
+
+        self.is_built: bool = False
+
+        self.W: Optional[tf.Variable] = None
+        self.b: Optional[tf.Variable] = None
+
+    def __call__(self, x_in: tf.Tensor) -> tf.Tensor:
+        """
+            Build tensor on first call.
+            Calculate output by 2D Depthwise Convolutions.
+        """
+
+        if not self.is_built:
+            filters_in = x_in.shape[3] if self.data_format == "NHWC" else x_in.shape[1]
+            filter_weights = self.kernel + (filters_in, self.depth_multiplier)
+
+            self.W = tf.Variable(
+                tf.random.normal(filter_weights),
+                trainable = True,
+                dtype = tf.float32,
+                name = "dwise_conv2d_filters"
+            )
+            self.b = tf.Variable(
+                tf.random.normal((filters_in * self.depth_multiplier,)),
+                trainable = True,
+                dtype = tf.float32,
+                name = "dwise_conv2d_bias"
+            )
+            self.is_built = True
+
+        if self.use_bias:
+            return tf.nn.depthwise_conv2d(
+                x_in, self.W, self.stride, self.padding,
+                self.data_format, self.dilation, name = "dwise_conv2d_conv"
+            ) + self.b
+        else:
+            return tf.nn.depthwise_conv2d(
+                x_in, self.W, self.stride, self.padding,
+                self.data_format, self.dilation, name = "dwise_conv2d_conv"
             )
