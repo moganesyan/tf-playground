2908ed878e183bfd57ecf86b344dfed6dbce7e4a
moganesyan <mishaogan1@hotmail.com>
2022-05-15 21:46:07 +0100

    tcn architecture defined

--- tensorflow/004_tcn/tcn.py
+++ tensorflow/004_tcn/tcn.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 import tensorflow as tf
 
 from layers import Dense, GlobalMaxPooling1D
@@ -11,10 +13,14 @@ class TCN(tf.Module):
 
     def __init__(self,
                  num_classes: int,
+                 architecture: List[Dict],
                  name: str = None) -> None:
         """
             args:
                 num_classes: int - Number of output classes.
+                architecture: List[Dict] - Architecture defined in block format.
+                    Example:
+                        {'kernel': 6, 'num_layers': 9, 'num_channels': 10 , 'dropout': 0.05}
                 name: str - Name of the model.
             returns:
                 None
@@ -22,33 +28,70 @@ class TCN(tf.Module):
 
         super(TCN, self).__init__(name)
         self._num_classes = num_classes
+        self._architecture = architecture
 
-        self._block_1 = ResidualTCNBlock(100, 2, 2, 0.1, name = 'tcn_block_1')
-        self._block_2 = ResidualTCNBlock(100, 2, 4, 0.1, name = 'tcn_block_2')
-        self._block_3 = ResidualTCNBlock(200, 3, 8, 0.1, name = 'tcn_block_3')
-        self._block_4 = ResidualTCNBlock(200, 3, 16, 0.1, name = 'tcn_block_4')
-        self._maxpool = GlobalMaxPooling1D(name = 'global_max_pool')
-        self._fc1 = Dense(num_classes, name = 'out_classes')
+        # placeholder for graph
+        self._layers = []
 
-    @tf.function(input_signature = [tf.TensorSpec(shape=(None, 100, 50), dtype=tf.float32)])
-    def __call__(self,
-                 x_in: tf.Tensor,
-                 training: bool = False) -> tf.Tensor:
+        # flag to build graph on first call
+        self._is_built = False
+
+    def _build_architecture(self) -> None:
         """
+            Build graph from the architecture definition.
+            Store created layers in a list.
+
+            args:
+                None
+            returns:
+                None
+        """
+
+        # build convolutional blocks
+        for block_idx, block_params in enumerate(self._architecture):
+            for layer_idx in range(block_params["num_layers"]):
+                dilation_rate_current = 2 ** layer_idx
+                self._layers.append(
+                    ResidualTCNBlock(
+                        block_params["num_channels"],
+                        block_params["kernel"],
+                        dilation_rate_current,
+                        block_params["dropout"],
+                        name = f"residual_block_{layer_idx}"
+                    )
+                )
+        # global max pooling
+        self._layers.append(
+            GlobalMaxPooling1D(name = "global_maxpool")
+        )
+        # final dense layer
+        self._layers.append(
+            Dense(
+                self._num_classes,
+                name = "final_dense"
+            )
+        )
+
+    @tf.function(input_signature = [tf.TensorSpec(shape = (None, 20, 1), dtype = tf.float32)])
+    def __call__(self, x_in: tf.Tensor) -> tf.Tensor:
+        """
+            Build graph on first call.
+
             args:
                 x_in: tf.Tensor - Input tensor.
-                training: bool - Flag to toggle train time and inference time behaviour.
             returns:
-                x_out: tf.Tensor - Output tensor.
+                x_out: tf.Tesnor - Output tensor.
         """
 
-        x = self._block_1(x_in, training = training)
-        x = self._block_2(x, training = training)
-        x = self._block_3(x, training = training)
-        x = self._block_4(x, training = training)
-        x = self._maxpool(x)
-        x = self._fc1(x)
+        if not self._is_built:
+            self._build_architecture()
+            self._is_built = True
 
-        x_out = tf.nn.softmax(x, name = 'softmax')
+        for layer_idx, layer in enumerate(self._layers):
+            if layer_idx == 0:
+                x = layer(x_in)
+            else:
+                x = layer(x)
+        x_out = tf.nn.softmax(x, name = 'final_softmax')
 
         return x_out
